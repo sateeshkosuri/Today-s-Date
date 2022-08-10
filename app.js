9ee9614ff747e3cf773606666bfd5e63631f73f4
Sateesh <sateeshkosuri1997@gmail.com>
2022-08-10 06:13:03 +0000

    Tests for Today's Date

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (request, response) => {
+  const todayDate = new Date();
+  response.send(
+    `${todayDate.getDate()}-${
+      todayDate.getMonth() + 1
+    }-${todayDate.getFullYear()}`
+  );
+});
+
+app.listen(3000);
+
+module.exports = app;
